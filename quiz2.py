6a75bcb9e7c276c9159408b3fc38c6fea6affeed
ngungo16 <ann863@hotmail.com>
2013-10-31 11:48:57 -0700

    Update quiz2.py

--- quiz2.py
+++ quiz2.py
@@ -8,10 +8,10 @@
 
 
 from lab2 import n
- if n > 50 :
-  print ("greater than 50" n)
- elif n<50:
-  print ("less than 50" n)
- elif n == 50 :
-  print ( "equal to 50" n)
+if n > 50 :
+ print ' greater than 50:' ,n
+elif n < 50:
+ print 'less than 50:' ,n
+elif n == 50 :
+ print 'equal to 50:' ,n 
   
