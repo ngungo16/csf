5c030047f4ad6597f0ad6cad14a7a36b7c8d8e66
ngungo16 <ann863@hotmail.com>
2013-10-31 11:11:08 -0700

    Create quiz2.py

---
+++ quiz2.py
@@ -0,0 +1,17 @@
+# From a module called lab2
+# import a variable called n
+# print one of the following things:
+# "greater than 50"
+# "less than 50"
+# "equal to 50"
+#based on the value n
+
+
+from lab2 import n
+ if n > 50 :
+  print ("greater than 50" n)
+ elif n<50:
+  print ("less than 50" n)
+ elif n == 50 :
+  print ( "equal to 50" n)
+  
