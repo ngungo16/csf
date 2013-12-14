512fc59a9bf97571f4c94145a58feb73c53eb6ef
ngungo16 <ann863@hotmail.com>
2013-12-13 17:23:37 -0800

    hw8

--- hw6.py
+++ hw6.py
@@ -1,24 +1,26 @@
  
+from element import *
  
  # problem 3 Sorting Dictionary
  
  # Write a script that sorts the elements by mass (from elements.py)
  
- elements = OrderedDict.fromkeys('123456')
+elements2 = OrderedDict.fromkeys('123456')
 
- elements.move_to_end('2')
- ''.join(elements.keys())
- '134562'
- elements.move_to_end('2', last=False)
- ''.join(elements.keys())
- '213456'
+elements2.move_to_end('2')
+''.join(elements2.keys())
+'134562'
+elements2.move_to_end('2', last=False)
+''.join(elements2.keys())
+'213456'
   
-  # dictionary sorted by mass value
- OrderedDict(sorted(elements.items(), key=lambda t: t[1]))
+# dictionary sorted by mass value
+OrderedDict(sorted(elements2.items(), key=lambda t: t[1]))
 
  
 
-# Problem 4:Sorting lists
+
+ # Problem 4:Sorting lists
 # Write a script that sorts the radio active list alphabetically (radioactive.txt)
 
 for k in sorted(set(Radio_Active)):
